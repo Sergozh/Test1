941ce7c81292fbfafefceb1c8f811bf71c3a43c8
Sergey <skorneev@me.com>
2012-04-11 12:51:27 +0400

    1st

---
+++ githello/githelloTests/githelloTests.h
@@ -0,0 +1,13 @@
+//
+//  githelloTests.h
+//  githelloTests
+//
+//  Created by Сергей Корнеев on 11.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface githelloTests : SenTestCase
+
+@end
---
+++ githello/githelloTests/githelloTests.m
@@ -0,0 +1,32 @@
+//
+//  githelloTests.m
+//  githelloTests
+//
+//  Created by Сергей Корнеев on 11.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "githelloTests.h"
+
+@implementation githelloTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in githelloTests");
+}
+
+@end
